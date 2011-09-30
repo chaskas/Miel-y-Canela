066100a0634f40aa0c1dc5660f0bb26e3721a21a
Rodrigo Campos H <mr.chaskas@gmail.com>
2011-09-30 14:04:42 -0300

    listo slider, falta poner las imagenes definitivas

--- apps/frontend/modules/static/templates/homeSuccess.php
+++ apps/frontend/modules/static/templates/homeSuccess.php
@@ -1,8 +1,50 @@
+<?php use_javascript('easySlider1.5.js'); ?>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $("#slider").easySlider({
+      auto: true,
+      continuous: true
+    });
+  });
+</script>
 <div class="new-ref">
-  <h3>Nejnovejsi reference</h3>
-  <p>&quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pulvinar sapien sed pede.&quot;</p>
-  <a href="#"><img src="./images/ref.jpg" width="860" height="260" alt="" /></a>
-</div><!-- end: .new-ref -->     
+  <div id="banner">
+  <div id="slider">
+    <ul>
+      <li>
+        <img src="./images/slide1.jpg" width="860" height="260" alt="" />
+        <div class="info">
+          <div>
+            <h1>Miel y Canela</h1>
+            <h3>Panader&iacute;a y Pasteler&iacute;a</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="./images/slide2.jpg" width="860" height="260" alt="" />
+        <div class="info">
+          <div>
+            <h1>Miel y Canela</h1>
+            <h3>Panader&iacute;a y Pasteler&iacute;a</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+          </div>
+        </div>
+      </li>
+      <li>
+        <img src="./images/ref.jpg" width="860" height="260" alt="" />
+        <div class="info">
+          <div>
+            <h1>Miel y Canela</h1>
+            <h3>Panader&iacute;a y Pasteler&iacute;a</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+  </div>
+</div>   
 
 <div id="cook">
   <h1>Ruce ze stolu, servirujeme profesionalni webove stranky!</h1>
